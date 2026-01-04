a236a25eeabe6fd69fe938f63de5a2ecb83e03f1
Igor <dsw51706@student.dsw.edu.pl>
2026-01-04 13:45:09 +0100

    dodanie env i przygotowanie mapbox API

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
   @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  MapboxMap? mapboxMap;
+
+  _onMapCreated(MapboxMap map) {
+    mapboxMap = map;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final token = dotenv.env['MAPBOX_ACCESS_TOKEN'] ?? '';
+
+    if (token.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Mapa'),
+        ),
+        body: const Center(
+          child: Text(
+            'Brakuje MAPBOX_ACCESS_TOKEN w pliku .env',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
-      body: const SizedBox.expand(),
+      body: MapWidget(
+        key: const ValueKey("mapWidget"),
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(19.9449799, 50.0646501)),
+          zoom: 10.0,
+        ),
+        onMapCreated: _onMapCreated,
+      ),
     );
   }
 }
